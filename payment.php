4a141a4873dd2a2e5f2c9247d10951afe803f415
HexTeto <syui.chu@gmail.com>
2016-11-20 17:12:28 +0800

    doc payment

---
+++ payment.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <title>支付页</title>
+    </head>
+    <body>
+        <h4>请扫描下方二维码完成支付</h4>
+        <br/>
+        <img src="images/qrcode.png" alt="QRCode">
+        <br/>
+        <a href="https://unisvnet.kf5.com">支付遇到问题?</a>
+    </body>
+</html>
